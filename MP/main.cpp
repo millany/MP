35c04439b0835b06cb722d9f0518e84667b7852a
Justin Lance Roma <justinlanceroma@yahoo.com>
2018-04-12 22:47:01 +0800

    Initial Commit

---
+++ MP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MP
+//
+//  Created by Justin Lance Roma on 04/12/2018.
+//  Copyright © 2018 Justin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
